8fd2a1a487d1f606033ba954ebc3d1c66d6551bc
Janek <jan.piotr.latko@gmail.com>
2021-09-21 12:11:04 +0200

    fix vis

--- src/utils.py
+++ src/utils.py
@@ -10,11 +10,13 @@ def mean_flat(tensor):
 
 
 def save_img(x, path):
+    x = x.transpose(1, 2, 0)
     if x.shape[0] == 1:
         # img = Image.fromarray(x[0, :, :], "L")
-        plt.imshow(x[0, :, :], cmap="gray")
+        plt.imshow(x[:,:,0], cmap="gray")
         plt.savefig(path, bbox_inches="tight", pad_inches=0)
     else:
-        img = Image.fromarray(x, "RGB")
-        # plt.imshow(x.reshape((32, 32, 3))) #rows, columns, channels
-        img.save(path)
+        # img = Image.fromarray(x, "RGB")
+        plt.imshow(x) #rows, columns, channels
+        # img.save(path)
+        plt.savefig(path, bbox_inches="tight", pad_inches=0)

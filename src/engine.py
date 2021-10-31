34176a8d75babad64ab6a9c99a65d57e05180643
Janek <jan.piotr.latko@gmail.com>
2021-10-31 17:55:07 +0100

    logging

--- src/engine.py
+++ src/engine.py
@@ -192,6 +192,14 @@ class Engine(pl.LightningModule):
 
         total_norm = self.compute_grad_norm(self.model.parameters())
         self.log("loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        # log loss per Q
+        for i in range(4):
+            self.log(
+                f"running_loss_q{i+1}",
+                self.loss_per_t.get_avg_in_range(max(1, int(i*self.diffusion_steps/4)),
+                                                       int((i+1)*self.diffusion_steps/4)),
+                on_step=True, on_epoch=False, prog_bar=False
+            )
         self.log(
             "total_grad_norm_L2",
             total_norm,

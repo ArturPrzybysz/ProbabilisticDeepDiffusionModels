87159d88ee3f008949cfb61e2b5366d71e936115
Janek <jan.piotr.latko@gmail.com>
2021-11-02 08:28:41 +0100

    fix importance

--- src/sampling/importance_sampler.py
+++ src/sampling/importance_sampler.py
@@ -23,7 +23,8 @@ class ImportanceSampler:
 
     def __call__(self, batch_size, device):
         if self.is_ready():
-            p = self.loss_per_t.avg_sq_per_step / self.loss_per_t.avg_sq_per_step.sum()
+            p = self.loss_per_t.avg_sq_per_step + 1e-6 # in case of 0s
+            p = p / p.sum()
             indices = np.random.choice(self.diffusion_steps-1, size=(batch_size,), p=p) + 1 # +1 because of 1 indexed t
             weights = 1 / (p[indices] * batch_size)
             t = torch.from_numpy(indices).long().to(device)
